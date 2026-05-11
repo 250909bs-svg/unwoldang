d3b33169544395192f6475e43b7cfb6ddd0eafb5
unwoldang <250909bs@gmail.com>
2026-05-12 02:19:14 +0900

    Update legal policies for review

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,13 +17,16 @@ export default function Footer() {
           <p>통신판매업 신고번호: 제 2025-서울구로-0005호</p>
           <p>주소: 서울특별시 구로구 구로동180-3</p>
           <p>상호명: 케이컴퍼니(운월당)</p>
-          <p>고객센터: 250909bs@gmail.com | 개인정보보호책임자: 차민호</p>
+          <p>고객센터 전화: 050420111894 | 이메일: 250909bs@gmail.com</p>
+          <p>개인정보보호책임자: 차민호</p>
         </div>
 
         <div className="site-footer-links compact">
           <Link to="/terms">이용약관</Link>
           <span>|</span>
           <Link to="/privacy">개인정보처리방침</Link>
+          <span>|</span>
+          <Link to="/refund">환불정책</Link>
         </div>
       </div>
     </footer>
